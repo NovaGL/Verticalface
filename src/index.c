5a174ee2f71dd35cc3b9432bb8d81073cefb841e
NovaGL <gl1@live.com>
2016-02-10 16:18:47 +1100

    Latest release

--- src/index.c
+++ src/index.c
@@ -2,12 +2,15 @@
 #define KEY_COLOR_RED     0
 #define KEY_COLOR_GREEN   1
 #define KEY_COLOR_BLUE    2
+#define KEY_LEADING_ZERO  3
 
 static Window *s_main_window;
 static TextLayer *s_hour_layer,*s_minute_layer;
 static GFont s_hour_font, s_minute_font;
 GColor time_color;
+static bool leading_zero = 0;
 static int red, green, blue;
+static int padding = 0;
 
 static void update_time() {
         // Get a tm structure
@@ -18,9 +21,22 @@ static void update_time() {
     static char bufferhr[] = "00";
     static char buffermin[] = "00";
     
-    // Write the current hours and minutes into the buffer
-    // Use 12 hour format
-    strftime(bufferhr, sizeof("00"), "%I", tick_time);
+    // Write the current hours and minutes into the buffer   
+  
+  if (leading_zero == 1) {
+    if(clock_is_24h_style()) {
+      strftime(bufferhr, sizeof(" 0"), "%k", tick_time);
+    } else {
+      strftime(bufferhr, sizeof(" 0"), "%l", tick_time);
+    }
+  } else {
+     if(clock_is_24h_style()) {
+      strftime(bufferhr, sizeof("00"), "%H", tick_time);
+    } else {
+      strftime(bufferhr, sizeof("00"), "%I", tick_time);
+    }
+  }
+    
     strftime(buffermin, sizeof("00"), "%M", tick_time);
     
     
@@ -37,17 +53,16 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
 static void main_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
-  const int text_height = 47;
-  
-  s_hour_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_SIPLE_BOLD_47));
-  s_minute_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_SIPLE_LITE_47));
+  const int text_height = 55;
+    
+  s_hour_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_SIPLE_BOLD_55));
+  s_minute_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_SIPLE_LITE_55));
   
   //Set Hour Layer
     s_hour_layer = text_layer_create(
-    GRect(bounds.origin.x, (bounds.size.h - 92) / 2, bounds.size.w, text_height));
+    GRect(bounds.origin.x, (bounds.size.h - 110) / 2, bounds.size.w, text_height));
     text_layer_set_background_color(s_hour_layer, GColorClear);  
     text_layer_set_text_color(s_hour_layer, GColorWhite);
-    text_layer_set_text(s_hour_layer, "00");
     text_layer_set_font(s_hour_layer, s_hour_font);
     text_layer_set_text_alignment(s_hour_layer, GTextAlignmentCenter);
     layer_add_child(window_layer, text_layer_get_layer(s_hour_layer));
@@ -83,11 +98,12 @@ static void main_window_load(Window *window) {
 void in_received_handler(DictionaryIterator *received, void *context) {
   
     ////// Colour Scheme
-    Tuple *color_red_t = dict_find(received, KEY_COLOR_RED);
-    Tuple *color_green_t = dict_find(received, KEY_COLOR_GREEN);
-    Tuple *color_blue_t = dict_find(received, KEY_COLOR_BLUE);
-  
-    if(color_red_t && color_green_t && color_blue_t) {
+    Tuple *color_red_t    = dict_find(received, KEY_COLOR_RED);
+    Tuple *color_green_t  = dict_find(received, KEY_COLOR_GREEN);
+    Tuple *color_blue_t   = dict_find(received, KEY_COLOR_BLUE);
+    Tuple *leading_zero_t = dict_find(received, KEY_LEADING_ZERO);
+   
+  if(color_red_t && color_green_t && color_blue_t) {
     // Apply the color if available
       APP_LOG(APP_LOG_LEVEL_INFO, "Set time font color");
     #if defined(PBL_BW)
@@ -105,6 +121,20 @@ void in_received_handler(DictionaryIterator *received, void *context) {
     #endif
   }  
 
+  //Hide Leading Zero
+   if (leading_zero_t) {
+  	
+  	if (strcmp(leading_zero_t->value->cstring, "on") == 0) {
+  		APP_LOG(APP_LOG_LEVEL_INFO, "Hide Leading Zero");
+  		leading_zero = 1;
+      padding = 5;
+  	} else {
+  		APP_LOG(APP_LOG_LEVEL_INFO, "Show Leading Zero");
+  		leading_zero = 0;
+      padding = 0;
+  	}     
+   }
+  
   
   update_time();
   
--- src/js/app.js
+++ src/js/app.js
@@ -2,17 +2,22 @@ Pebble.addEventListener("ready", function(e) {});
 
 Pebble.addEventListener("showConfiguration",
   function(e) {
-    Pebble.openURL("http://192.168.1.42");
+    Pebble.openURL("https://rawgit.com/NovaGL/Verticalface/master/config/index.html");
   }
 );
 Pebble.addEventListener("webviewclosed",
   function(e) {
     var configuration = JSON.parse(e.response);
     var backgroundColor = configuration.background_color;
+    
+    if (!configuration.leading_zero) {
+      configuration.leading_zero = "off";
+    }
     var dict = {};
         dict.KEY_COLOR_RED = parseInt(backgroundColor.substring(2, 4), 16);
         dict.KEY_COLOR_GREEN = parseInt(backgroundColor.substring(4, 6), 16);
         dict.KEY_COLOR_BLUE = parseInt(backgroundColor.substring(6), 16);
+        dict.KEY_LEADING_ZERO = configuration.leading_zero;
     
     console.log(e.response);
     //Pebble.sendAppMessage(configuration);
